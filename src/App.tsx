408e1b4821197bc949e4b420f30aefcd83dec872
Soumen Khara <soumen2015.s.k@gmail.com>
2022-09-14 12:12:03 +0530

    feat: added tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-function App({ b }: { b?: number }) {
-    const [count, setCount] = useState(0);
-    const a: A = { name: "Soumen Khara" };
-    console.log(a);
-    useEffect(() => {
-        setCount(b || 0);
-    }, [b]);
-
-    return <div className="App"></div>;
+import React from "react";
+function App() {
+    return <></>;
 }
 
 export default App;
